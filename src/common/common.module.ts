bbda9334ee217d949af06d017deaa007c6078454
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-28 22:26:08 -0500

    feat: create provider neo4j

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,6 +2,7 @@ import { Module, Global } from '@nestjs/common';
 import { TypeOrmConfigService } from './providers/ormconfig.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/enviroment.config'
+import { Neo4jProvider } from './providers/neo4j.provider';
 
 @Global()
 @Module({
@@ -12,7 +13,7 @@ import configuration from './config/enviroment.config'
       isGlobal: true, 
     }),
   ],
-  providers: [TypeOrmConfigService],
+  providers: [TypeOrmConfigService, Neo4jProvider],
   exports: [TypeOrmConfigService]
 })
 export class CommonModule {}
---
+++ src/common/providers/neo4j.provider.ts
@@ -0,0 +1,7 @@
+ 
+import neo4j from "neo4j-driver";
+
+export const Neo4jProvider = {
+  provide: "Neo4j",
+  useFactory: () => neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j","1234"))
+ }
