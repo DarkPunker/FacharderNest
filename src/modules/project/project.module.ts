982462881bda07e81f67283e40e6394981e5550b
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-29 17:51:30 -0500

    import neo module

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -4,10 +4,11 @@ import { ProjectService } from './project.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../../entities/project.entity';
 import { Document } from '../../entities/document.entity';
+import { NeoModule } from '../neo4j/neo.module';
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Project, Document])],
+    imports: [NeoModule, TypeOrmModule.forFeature([Project, Document])],
     controllers: [ProjectController],
     providers: [ProjectService]
 })
