8bcbe568977d382e7f45a2645c5f1684a7c68699
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-29 15:34:59 -0500

    fix: correctly bug ''," in query and change return recods

--- src/modules/neo4j/role.neo4j.service.ts
+++ src/modules/neo4j/role.neo4j.service.ts
@@ -5,19 +5,19 @@ import { Role } from "../../entities/rol.entity";
 export class RoleNeoService{
   constructor(@Inject('Neo4j') private readonly neo4j){}
 
-  public async createRole(role: Role): Promise<any> {
+  public async createRole(role): Promise<any> {
     const session = this.neo4j.session();
     const query = `
       CREATE (r:Role {
         id: '${role.idRol}', 
-        name: '${role.name}',
+        name: '${role.name}'
       }) return r
     `;
     return session
         .run(query)
         .then((result) => {
         session.close();
-        return result.records.map(record => record.toObject());
+        return result.records.map(record => record.toObject()['r']['properties']);
       })
         .catch((error) =>
         Promise.reject(new BadRequestException(error))
