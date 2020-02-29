d437471521238426d0fa6011fe1def0f14caece1
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-29 17:44:10 -0500

    feat: create assignProjectToTeam for create relationship Document with Project and create addDocumentToProject for assign relationship of Project wirh Document

--- src/modules/neo4j/project.neo4j.service.ts
+++ src/modules/neo4j/project.neo4j.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, Inject, BadRequestException } from "@nestjs/common";
 import { Project } from "../../entities/project.entity";
+import { IRelationship } from "./interfaces/relationship.interface";
+import { RelationshipService } from "./relationship.service";
 
 @Injectable()
-export class ProjectNeoService{
-  constructor(@Inject('Neo4j') private readonly neo4j){}
+export class ProjectNeoService {
+  constructor(
+    @Inject('Neo4j') private readonly neo4j,
+    private readonly relationshipService: RelationshipService
+  ) { }
 
   public async createProject(project: Project): Promise<any> {
     const session = this.neo4j.session();
@@ -17,14 +22,31 @@ export class ProjectNeoService{
       }) return u
     `;
     return session
-        .run(query)
-        .then((result) => {
+      .run(query)
+      .then((result) => {
         session.close();
-        return result.records.map(record => record.toObject());
+        return result.records.map(record => record.toObject()['u']['properties']);
       })
-        .catch((error) =>
+      .catch((error) =>
         Promise.reject(new BadRequestException(error))
       )
   }
 
+  public async assignProjectToTeam(idProject: number, idTeam: number): Promise<any> {
+    const rela = {
+      nodeA: { id: String(idTeam), type: 'Team' },
+      nodeB: { id: String(idProject), type: 'Project' },
+      name: "HAS_ASSIGNED"
+    } as IRelationship;
+    await this.relationshipService.createRelationship(rela);
+  }
+
+  public async addDocumentToProject(idProject: number, idDocument: number): Promise<any> {
+    const rela = {
+      nodeA: { id: String(idProject), type: 'Project' },
+      nodeB: { id: String(idDocument), type: 'Document' },
+      name: "HAS_A"
+    } as IRelationship;
+    await this.relationshipService.createRelationship(rela);
+  }
 }
