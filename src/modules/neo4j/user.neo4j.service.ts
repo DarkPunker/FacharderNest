2bf3ebd54911dad9570abf581c0b516d549b9b22
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-29 15:36:25 -0500

    feat: create method for set role to user and relationship

--- src/modules/neo4j/user.neo4j.service.ts
+++ src/modules/neo4j/user.neo4j.service.ts
@@ -1,9 +1,18 @@
 import { Injectable, Inject, BadRequestException } from "@nestjs/common";
 import { User } from "../../entities/user.entity";
+import { RelationshipService } from "./relationship.service";
+import { RoleNeoService } from "./role.neo4j.service";
+import { Role } from "../../entities/rol.entity";
+import {IRelationship} from './interfaces/relationship.interface';
+import { userInfo } from "os";
 
 @Injectable()
 export class UserNeoService{
-  constructor(@Inject('Neo4j') private readonly neo4j){}
+  constructor(
+    @Inject('Neo4j') private readonly neo4j,
+    private readonly roleService: RoleNeoService,
+    private readonly relationshipService: RelationshipService
+    ){}
 
   public async createUser(user: User): Promise<any> {
     const session = this.neo4j.session();
@@ -20,7 +29,7 @@ export class UserNeoService{
         .run(query)
         .then((result) => {
         session.close();
-        return result.records.map(record => record.toObject());
+        return result.records.map(record => record.toObject()['u']['properties']);
       })
         .catch((error) =>
         Promise.reject(new BadRequestException(error))
@@ -31,7 +40,7 @@ export class UserNeoService{
     const session = this.neo4j.session();
     const query = `
       CREATE (u:Client {
-        idUser: '${user.idUser}', 
+        id: '${user.idUser}', 
         username: '${user.username}', 
         firstname:'${user.firstname}',
         lastname: '${user.lastname}',
@@ -42,10 +51,21 @@ export class UserNeoService{
         .run(query)
         .then((result) => {
         session.close();
-        return result.records.map(record => record.toObject());
+        return result.records.map(record => record.toObject()['u']['properties']);
       })
         .catch((error) =>
         Promise.reject(new BadRequestException(error))
       )
+  }
+
+  public async createUserAndRole(user: User): Promise<any> {
+    const us = await this.createUser(user);
+    const role = await this.roleService.createRole(user.roles);
+    const rela = {
+      nodeA: {id: String(us[0].id), type: 'User'},
+      nodeB: {id: String(role[0].id), type: 'Role'},
+      name: "HAS_A_ROLE"
+    } as IRelationship;
+    await this.relationshipService.createRelationship(rela); 
   }
 }
