606a66a1cfe3b20220e2160f93c7de34ebef06a2
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-02-29 20:08:59 -0500

    feat: import Team entity in module

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../../entities/project.entity';
 import { Document } from '../../entities/document.entity';
 import { NeoModule } from '../neo4j/neo.module';
+import { Team } from '../../entities/team.entity';
 
 
 @Module({
-    imports: [NeoModule, TypeOrmModule.forFeature([Project, Document])],
+    imports: [NeoModule, TypeOrmModule.forFeature([Project, Document, Team])],
     controllers: [ProjectController],
     providers: [ProjectService]
 })
